d3ab1bf6f052dc23b801c18b4e7ce78f0d9397e3
Mcelest19 <mrn.celestino@gmail.com>
2023-02-01 14:52:24 +0100

    update  scripts.js
    was added a list of pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world')
-let favoriteFood  = 'Cakes';
-document.write(favoriteFood);
+let pokemonList =[
+    {name: "Bulbasaur", height: 0.7, type: ["grass","poison"]},
+    {name: "Squirtle", height: 0.5, type:"water"},
+    {name: "Jigglypuff", height: 0.5, type: ["fairy","normal"]}
+]
