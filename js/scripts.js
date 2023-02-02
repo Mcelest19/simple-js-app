6b136f46710033498abe4a13f66bd81401de13f8
Mcelest19 <mrn.celestino@gmail.com>
2023-02-02 13:44:33 +0100

    general updates
    1. added images
    2. added script for pokemon list
    3.update html and css

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,13 @@ let pokemonList =[
     {name: "Bulbasaur", height: 0.7, type: ["grass","poison"]},
     {name: "Squirtle", height: 0.5, type:"water"},
     {name: "Jigglypuff", height: 0.5, type: ["fairy","normal"]}
-]
+];
+
+for (let i=0; i<pokemonList.length; i++ ){
+    if (pokemonList[i].height > 0.5){
+        document.write('<p>'+ pokemonList[i].name + ' (height:' + pokemonList[i].height + ')' + ' - is a big pokemon!</p>'); 
+} else {
+    document.write('<p>'+ pokemonList[i].name + ' (height:' + pokemonList[i].height + ')</p>');
+}
+}
+
