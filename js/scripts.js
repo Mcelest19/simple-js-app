53e5749d5823960e26b16002f1f69b5a114891eb
Mcelest19 <mrn.celestino@gmail.com>
2023-02-03 13:46:14 +0100

    Ex._1.5_Part_1
    implemented forEach() Loop

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,19 @@ let pokemonList =[
     {name: "Jigglypuff", height: 0.5, type: ["fairy","normal"]}
 ];
 
+/*
+ For Loop
+
 for (let i=0; i<pokemonList.length; i++ ){
     if (pokemonList[i].height > 0.5){
         document.write('<p>'+ pokemonList[i].name + ' (height:' + pokemonList[i].height + ')' + ' - is a big pokemon!</p>'); 
 } else {
     document.write('<p>'+ pokemonList[i].name + ' (height:' + pokemonList[i].height + ')</p>');
 }
+  */
+ 
+// forEach() Loop
+function myLoopFunction(pokemon){
+    document.write('<p>'+ pokemon.name + ' ' + pokemon.height + ' ' + pokemon.type + ' ' + '</p>')
 }
-
+pokemonList.forEach(myLoopFunction)
