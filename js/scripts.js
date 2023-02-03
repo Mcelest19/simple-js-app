52bcc8f04a9b32954921a5c4db36ca2a63980028
Mcelest19 <mrn.celestino@gmail.com>
2023-02-03 15:34:13 +0100

    Ex1.5_Part_2
    implement IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
-let pokemonList =[
-    {name: "Bulbasaur", height: 0.7, type: ["grass","poison"]},
-    {name: "Squirtle", height: 0.5, type:"water"},
-    {name: "Jigglypuff", height: 0.5, type: ["fairy","normal"]}
-]
+let pokemonRepository = (function (){
+    let pokemonList =[
+        {name: "Bulbasaur", height: 0.7, type: ["grass","poison"]},
+        {name: "Squirtle", height: 0.5, type:"water"},
+        {name: "Jigglypuff", height: 0.5, type: ["fairy","normal"]}
+    ]
+        
+    function add(pokemon){
+        pokemonList.push(pokemon);
+    }
+
+    function getAll(){
+        return pokemonList;
+    }     
+
+    return {
+        add: add,
+        getAll: getAll
+    }
+})();
+
 /*
  !!"For" Loop!!
 
@@ -16,14 +32,15 @@ for (let i=0; i<pokemonList.length; i++ ){
 
 // !!forEach() Loop!!
 
-function myLoopFunction(pokemon){
+pokemonRepository.getAll().forEach(function (pokemon) {    
     if (pokemon.height> 0.5){
         document.write('<p>'+ pokemon.name + ' (height:' + pokemon.height + ')' + ' - is a big pokemon!</p>'); 
 }else {
     document.write('<p>'+ pokemon.name + ' (height:' + pokemon.height + ')</p>');
 }
-}
-pokemonList.forEach(myLoopFunction)
+});
+
+      
 
 
   
